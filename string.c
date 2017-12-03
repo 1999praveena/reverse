6d0482c9dc0b170b21fddc14e06b4f05832159e8
1999praveena <praveenaarokiaraj@gmail.com>
2017-12-03 12:48:24 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char str[10];
+printf("enter string");
+scanf("%s",str);
+printf("\n the reversed string is %s",strrev(str));
+getch();
+}
